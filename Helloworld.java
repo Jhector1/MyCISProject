2b7a7f6ba4032ce27171922807108fa3a9ec5426
Jean Yves Hector <jeanyveshector@gmail.com>
2022-03-23 19:13:58 -0500

    g

---
+++ Helloworld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		printSomething();
+	}
+
+	static void printSomething() {
+	System.out.println("I made it");
+	}
+}
